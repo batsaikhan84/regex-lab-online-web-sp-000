fbfb1bf9b1cf3f4c477ca3ba3dac08216cb59443
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-12 15:22:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[aeiouAEIOU]/)
-    true
-  else
-    false
+  !(word.scan(/\b[aeiouAEIOU]/))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
